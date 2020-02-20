cf0fe327205bb5cdd024d03d27466e52980a9482
Joshua Frkuska <j.frkuska@gmail.com>
2020-02-20 11:50:31 +0900

    Update copyright information
    Signed-off-by: Joshua Frkuska <j.frkuska@gmail.com>

--- switch_master.py
+++ switch_master.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#    Copyright 2019 Joshua Frkuska
+#    Copyright 2019, 2020 Joshua Frkuska
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
